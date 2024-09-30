6f214d556bf06d95aae0f80f10a399fc30dbe7c4
Sandeeppawar007 <145098014+Sandeeppawar007@users.noreply.github.com>
2024-09-30 12:35:00 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -4,7 +4,30 @@
  */
 var romanToInt = function(s) {
     
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    
 };
 
-
+ let total = 0;
+    
+   
+    for (let i = 0; i < s.length; i++) {
+        /
+        if (i + 1 < s.length && romanValues[s[i]] < romanValues[s[i + 1]]) {
+            total -= romanValues[s[i]];
+        } else {
+            
+            total += romanValues[s[i]];
+        }
+    }
+    
+    return total;
+};
 module.exports={romanToInt}
