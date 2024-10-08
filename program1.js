bd4b77d443567a76328cea090dcba39306b0e18a
Sandeeppawar007 <145098014+Sandeeppawar007@users.noreply.github.com>
2024-10-08 14:26:41 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,39 +1,3 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    r isValid = function(s) {
-   
-    let stack = [];
 
-   
-    const matchingBrackets = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    
-};
-
-         for (let char of s) {
-        
-        if (char in matchingBrackets) {
-          
-            let topElement = stack.length ? stack.pop() : '#';
-            
-           
-            if (topElement !== matchingBrackets[char]) {
-                return false;
-            }
-        } else {
-  
-            stack.push(char);
-        }
-    }
-
- 
-    return stack.length === 0;
-};
-module.exports = { isValid };
 
 
